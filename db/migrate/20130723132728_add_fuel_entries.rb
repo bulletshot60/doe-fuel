7d0f3383416f8957e4c696b18d5e9fbf124a472c
Ethan McGee <bulletshot60@gmail.com>
2013-07-23 09:52:01 -0400

    wrong data type

--- db/migrate/20130723132728_add_fuel_entries.rb
+++ db/migrate/20130723132728_add_fuel_entries.rb
@@ -1,22 +1,22 @@
 class AddFuelEntries < ActiveRecord::Migration
   def change
-  	add_column :regular_prices, :colorado, :string
-  	add_column :regular_prices, :florida, :string
-  	add_column :regular_prices, :massachusetts, :string
-  	add_column :regular_prices, :minnesota, :string
-  	add_column :regular_prices, :new_york, :string
-  	add_column :regular_prices, :ohio, :string
-  	add_column :regular_prices, :texas, :string
-  	add_column :regular_prices, :washington, :string
-  	add_column :regular_prices, :boston, :string
-  	add_column :regular_prices, :chicago, :string
-  	add_column :regular_prices, :cleveland, :string
-  	add_column :regular_prices, :denver, :string
-  	add_column :regular_prices, :houston, :string
-  	add_column :regular_prices, :los_angeles, :string
-  	add_column :regular_prices, :miami, :string
-  	add_column :regular_prices, :new_york_city, :string
-  	add_column :regular_prices, :san_francisco, :string
-  	add_column :regular_prices, :seattle, :string
+  	add_column :regular_prices, :colorado, :float
+  	add_column :regular_prices, :florida, :float
+  	add_column :regular_prices, :massachusetts, :float
+  	add_column :regular_prices, :minnesota, :float
+  	add_column :regular_prices, :new_york, :float
+  	add_column :regular_prices, :ohio, :float
+  	add_column :regular_prices, :texas, :float
+  	add_column :regular_prices, :washington, :float
+  	add_column :regular_prices, :boston, :float
+  	add_column :regular_prices, :chicago, :float
+  	add_column :regular_prices, :cleveland, :float
+  	add_column :regular_prices, :denver, :float
+  	add_column :regular_prices, :houston, :float
+  	add_column :regular_prices, :los_angeles, :float
+  	add_column :regular_prices, :miami, :float
+  	add_column :regular_prices, :new_york_city, :float
+  	add_column :regular_prices, :san_francisco, :float
+  	add_column :regular_prices, :seattle, :float
   end
 end
